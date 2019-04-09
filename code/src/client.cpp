b796523102968e31ded8af2f39162feffe8b06f0
Piotr Ogrodnik <piotrekogrodnik81@gmail.com>
2019-04-09 22:39:45 +0200

    Adds basic tcp server and client

--- code/src/client.cpp
+++ code/src/client.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main() {
-  std::cout<<"client output"<<std::endl;
+#define MAX_BUF 100
 
-  return(0);
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int sockd;
+	int count;
+	struct sockaddr_in serv_name;
+	char buf[MAX_BUF];
+	int status;
+
+	if (argc < 3)
+	{
+		cout<<"Usage: %s ip_address port_number "<<argv[0]<<endl;
+		return(1);
+	}
+	/* create a socket */
+	sockd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockd == -1)
+	{
+		cout<<"Socket creation"<<endl;
+		return(1);
+	}
+
+	/* server address */ 
+	serv_name.sin_family = AF_INET;
+	inet_aton(argv[1], &serv_name.sin_addr);
+	serv_name.sin_port = htons(stoi(argv[2]));
+
+	/* connect to the server */
+	status = connect(sockd, (struct sockaddr*)&serv_name, sizeof(serv_name));
+	if (status == -1)
+	{
+		cout<<"Connection error"<<endl;
+		return(1);
+	}
+
+	count = read(sockd, buf, MAX_BUF);
+	write(1, buf, count);
+
+	close(sockd);
+	return 0;
 }
