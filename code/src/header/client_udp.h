b599751bfdef3eb24b55933b10cbecb58570f930
Krzykas <44545884+Krzykas@users.noreply.github.com>
2019-05-26 20:29:05 +0200

    Attempt to introduce classes

--- code/src/header/client_udp.h
+++ code/src/header/client_udp.h
@@ -1,6 +1,108 @@
-#include <iostream>
+#include <arpa/inet.h>
 
-int multiply(int a, int b)
+#include "core.h"
+
+class Client_udp : public Core
 {
-    return a * b;
+public:  
+  int portNumber;
+  int client_id;
+  size_t message_id;
+  Status client_status;
+
+public:
+  Client_udp(int argc, char* argv[]);
+  void handleSendMessage(struct addr_info& addr);
+  void handleGetMessage(struct addr_info& addr);
+  void handleHeartbeat(struct addr_info& addr);
+};
+
+Client_udp::Client_udp(int argc, char* argv[])
+{
+    this->portNumber = atoi(argv[2]);
+    this->client_id = stoi(argv[3]);
+    this->message_id = 0;
+    this->client_status = active;
+}
+
+void Client_udp::handleSendMessage(struct addr_info& addr)
+{
+    int messageFD = addr.fd;
+    struct sockaddr_in* servAddr = addr.addr_info;
+    socklen_t addrlen;
+    addrlen = sizeof(struct sockaddr_in);
+    char buffer[MAX_BUF];
+
+    struct checksum message_metadata;
+    
+    for(;;){
+        memset(buffer, 0, MAX_BUF);
+        message_id += 1;
+        message_metadata.clientID = client_id;
+        message_metadata.messageID = message_id;
+        message_metadata.statusID = active;
+
+        generate_message(buffer, message_metadata, "abcdef");
+        sendto(messageFD, (const char*)buffer, sizeof(buffer), 
+            0, (const struct sockaddr*)servAddr, 
+            addrlen); 
+
+        cout<<"Message sent"<<endl;
+
+        sleep(5);
+    }
+}
+
+void Client_udp::handleGetMessage(struct addr_info& addr)
+{
+  int messageFD = addr.fd;
+  struct sockaddr_in* servAddr = addr.addr_info;
+  size_t n;
+  socklen_t addrlen;
+  char buffer[MAX_BUF];
+
+  addrlen = sizeof(struct sockaddr_in);
+
+  for(;;){
+      n = recvfrom(messageFD, (char*)buffer, MAX_BUF, 
+                  0, (struct sockaddr*)servAddr, 
+                  &addrlen);
+
+        cout<<"Message from server: "<<buffer<<endl; 
+    }
+}
+
+void Client_udp::handleHeartbeat(struct addr_info& addr)
+{
+    int heartbeatFD = addr.fd;
+    struct sockaddr_in* servAddr = addr.addr_info;
+    size_t n;
+    socklen_t addrlen;
+    //Doesn't matter what the message is, we just want to send anything through
+    std::string msg = "Heartbeat";
+    char buffer[MAX_BUF];
+
+    addrlen = sizeof(struct sockaddr_in);    
+    
+    for(;;){
+        // send hello message to server 
+        sendto(heartbeatFD, (const char*)msg.c_str(), msg.length(), 
+            0, (const struct sockaddr*)servAddr, 
+            addrlen); 
+
+        cout<<"Heartbeat sent"<<endl;
+
+        n = recvfrom(heartbeatFD, (char*)buffer, MAX_BUF, 
+                        0, (struct sockaddr*)servAddr, 
+                        &addrlen);
+        if (errno == EAGAIN || errno == EWOULDBLOCK)
+        {
+            cout<<"CONNECTION LOST!!"<<endl;
+            throw "Connection lost";
+        }
+        
+        cout<<"\tHeartbeat response received: "<<buffer<<endl; 
+
+        sleep(8);
+    }
 }
--- code/src/header/server.h
+++ code/src/header/server.h
@@ -1,6 +1,81 @@
-#include <iostream>
+#include <fcntl.h>
+#include <errno.h>
 
-int add(int a, int b)
+#include "core.h"
+
+class Server : Core
 {
-    return a + b;
+public:
+    int portNumber;
+  
+    Server(int argc, char* argv[]);
+    void handleGetMessage(struct addr_info& addr);
+    void handleHeartbeat(struct addr_info& addr);
+};
+
+Server::Server(int argc, char* argv[])
+{
+    this->portNumber = atoi(argv[1]);
+}
+
+void Server::handleGetMessage(struct addr_info& addr)
+{
+    int messageFD = addr.fd;
+    struct sockaddr_in* peerAddr = addr.addr_info;
+	ssize_t n;
+	socklen_t addrlen;
+	struct checksum message_metadata;
+	string message_content;
+	char buffer[MAX_BUF];
+
+	addrlen = sizeof(struct sockaddr_in);
+
+    for (;;) 
+	{
+		memset(buffer, 0, strlen(buffer));
+		n = recvfrom(messageFD, buffer, sizeof(buffer), 0, 
+						(struct sockaddr*)peerAddr, &addrlen); 
+
+		get_message_metadata(message_metadata, buffer);
+		string message_content = get_message_content(buffer);
+        cout<<	"Message received: " <<endl\
+			<<	"\tClientID: "		<< message_metadata.clientID <<endl\
+			<<	"\tMessageID: "		<< message_metadata.messageID <<endl\
+			<<	"\tStatus: "		<< message_metadata.statusID <<endl\
+			<< 	"\tContent: "		<< message_content << endl;
+		
+		sendto(messageFD, (const char*)message_content.c_str(), message_content.length(),
+				0, (struct sockaddr*)peerAddr, 
+				addrlen);
+
+		cout<<"\tResponse sent"<<endl;
+    }
+}
+
+void Server::handleHeartbeat(struct addr_info& addr)
+{
+	int heartbeatFD = addr.fd;
+    struct sockaddr_in* servAddr = addr.addr_info;
+    size_t n;
+    socklen_t addrlen;
+    char buffer[MAX_BUF];
+
+    addrlen = sizeof(struct sockaddr_in);
+
+    for(;;){
+		memset(buffer, 0, strlen(buffer));
+		
+        n = recvfrom(heartbeatFD, (char*)buffer, MAX_BUF, 
+                        0, (struct sockaddr*)servAddr, 
+                        &addrlen);
+        
+        cout<<"Heartbeat received: "<<buffer<<endl; 
+
+
+        sendto(heartbeatFD, (const char*)buffer, strlen(buffer), 
+            0, (const struct sockaddr*)servAddr, 
+            addrlen); 
+
+        cout<<"\tHeartbeat response sent"<<endl;
+    }
 }
