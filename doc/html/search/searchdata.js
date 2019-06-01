f9d35ed0e4267d04ef18361333dfd8eca16f6532
Sebastian Kamoda <seba.kamoda@gmail.com>
2019-06-01 12:05:05 +0200

    Add generated documentation

---
+++ doc/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "acms",
+  1: "acms"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
